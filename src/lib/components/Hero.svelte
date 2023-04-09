5caa0c26a24d151d7d17c65a661f5690b97d9c12
Gabriel Escame <gabriel.escame@gmail.com>
2023-04-09 18:42:51 -0300

    feat: moving Hero component to its folder

---
+++ src/lib/components/Hero.svelte
@@ -0,0 +1,45 @@
+<script>
+  import { gsap } from 'gsap'
+  import { onMount } from 'svelte'
+
+  onMount(() => {
+    gsap.from('.hero__title', {
+      x: -200,
+      rotate: '5deg',
+      opacity: 0,
+      duration: 0.6
+    })
+  })
+</script>
+
+<section class="hero">
+  <h1 class="hero__title">
+    <strong>Hi,</strong> <br />
+    I'm
+    <strong
+      ><span class="hero__title hero__title--blue">Sk</span>ame,</strong
+    ><br />
+    <strong>Web</strong> developer
+  </h1>
+</section>
+
+<style lang="scss">
+  .hero {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100vh;
+
+    &__title {
+      color: $white;
+      font-style: italic;
+      font-weight: 100;
+
+      &--blue {
+        color: $blue;
+        font-weight: bolder;
+      }
+    }
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,44 +1,16 @@
 <script>
-	import { gsap } from 'gsap';
-	import { onMount } from 'svelte';
-
-	onMount(() => {
-		gsap.from('.landing__title', { x: -200, rotate: '5deg', opacity: 0, duration: 0.6 });
-	});
+  import Hero from '../lib/components/Hero.svelte'
 </script>
 
 <div class="landing">
-	<section class="landing__section">
-		<h1 class="landing__title">
-			<strong>Hi,</strong> <br />
-			I'm <strong><span class="landing__title landing__title--blue">Sk</span>ame,</strong><br />
-			<strong>Web</strong> developer
-		</h1>
-	</section>
+  <Hero />
 </div>
 
 <style lang="scss">
-	.landing {
-		background-color: $black;
-		font-size: 40px;
-		width: 100vw;
-		min-height: 100vh;
-
-		&__section {
-			display: flex;
-			justify-content: center;
-			align-items: center;
-			width: 100%;
-			height: 100vh;
-		}
-
-		&__title {
-			color: $white;
-			font-style: italic;
-
-			&--blue {
-				color: $blue;
-			}
-		}
-	}
+  .landing {
+    background-color: $black;
+    font-size: 40px;
+    width: 100vw;
+    min-height: 100vh;
+  }
 </style>

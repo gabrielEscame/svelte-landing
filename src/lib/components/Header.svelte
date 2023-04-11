2b1fae2dbf1295b97b6c62c9417ca2a41909401e
Gabriel Escame <gabriel.escame@gmail.com>
2023-04-11 01:19:54 -0300

    feat: adding header animation

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,3 +1,13 @@
+<script>
+  import { onMount } from 'svelte'
+  import { slideAnimation } from '$lib/utils/animation.js'
+
+  onMount(() => {
+    slideAnimation('.header__icon', { delay: 0.7 })
+    slideAnimation('.header__button', { delay: 0.7, x: 280 })
+  })
+</script>
+
 <div class="header">
   <a class="header__icon" href="#skame">SK</a>
   <button class="header__button" onclick={() => console.log('Hi buddy')}>
@@ -7,14 +17,15 @@
 
 <style lang="scss">
   .header {
-    display: flex;
     position: absolute;
     top: 0;
     left: 0;
-    width: 100%;
-    z-index: 3;
+    display: flex;
     justify-content: space-between;
+    overflow-x: hidden;
+    width: 100%;
     padding: 24px 80px;
+    z-index: 3;
     background: linear-gradient(
       180deg,
       #000000 5%,

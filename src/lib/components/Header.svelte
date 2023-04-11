9e6215e21aeb878717a2ba6f21c4ba6b97fe36a7
Gabriel Escame <gabriel.escame@gmail.com>
2023-04-11 01:01:08 -0300

    feat: adding header component

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,53 @@
+<div class="header">
+  <a class="header__icon" href="#skame">SK</a>
+  <button class="header__button" onclick={() => console.log('Hi buddy')}>
+    contacts
+  </button>
+</div>
+
+<style lang="scss">
+  .header {
+    display: flex;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    z-index: 3;
+    justify-content: space-between;
+    padding: 24px 80px;
+    background: linear-gradient(
+      180deg,
+      #000000 5%,
+      rgba(0, 0, 0, 0.5) 52%,
+      rgba(0, 0, 0, 0) 90%
+    );
+
+    &__icon {
+      position: relative;
+      font-size: 40px;
+      font-weight: 700;
+      font-style: italic;
+      text-decoration: none;
+      color: $blue;
+
+      &::before {
+        position: absolute;
+        bottom: -1px;
+        left: -20px;
+        content: '';
+        height: 2px;
+        width: 37px;
+        background-color: $blue;
+      }
+    }
+
+    &__button {
+      font-size: 16px;
+      font-weight: 600;
+      font-style: italic;
+      background-color: transparent;
+      border: none;
+      color: $blue;
+    }
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script>
   import Hero from '$lib/components/Hero.svelte'
+  import Header from '$lib/components/Header.svelte'
 </script>
 
 <div class="landing">
+  <Header />
   <Hero />
 </div>
 

59d30117d96dbf9d171b8b616a0e79c8f1d00d17
Gabriel Escame <gabriel.escame@gmail.com>
2023-04-13 04:04:58 -0300

    refactor: returning myJourney coponent to Home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,15 @@
 <script>
-  import { onMount } from 'svelte'
-
   import Hero from '$lib/components/Hero.svelte'
   import Header from '$lib/components/Header.svelte'
+  import MyJourney from '$lib/components/MyJourney.svelte'
 </script>
 
 <div class="landing">
   <Header />
   <Hero />
+  <div class="landing__container">
+    <MyJourney />
+  </div>
 </div>
 
 <style lang="scss">
@@ -16,5 +18,11 @@
     background-color: $dark;
     font-size: 40px;
     max-width: 100vw;
+
+    &__container {
+      max-width: 60%;
+      margin: auto;
+      padding: 0 24px;
+    }
   }
 </style>
